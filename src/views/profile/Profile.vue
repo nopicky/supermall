2e3b6ecc1a11802d440673b0b218329f15357829
nopicky <1666026435@qq.com>
2021-07-06 21:08:21 +0800

    supermall前期

---
+++ src/views/profile/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>我是profile组件</h2>
+<!--    <h2>{{$route.query.name}}</h2>-->
+<!--    <h2>{{$route.query.age}}</h2>-->
+    <!--    <h2>{{$route.query.height}}</h2>-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile"
+}
+</script>
+
+<style scoped>
+
+</style>
